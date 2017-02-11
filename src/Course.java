3b01a2fa683f0271ce6f68dd2aa4f431a68c686e
HeatherKozak <heather.a.kozak@gmail.com>
2017-02-11 17:26:00 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Heather on 2017-02-11.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    publc Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
